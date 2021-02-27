b404fa6eb2552cd12b3d6c7bf72d467aa1cd878d
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-02-26 21:05:31 -0500

    gamemanager functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	public List<GameObject> targets;
+	private float spawnRate;
 	// Start is called before the first frame update
 	void Start()
 	{
+		StartCoroutine(SpawnTarget());
+	}
 
+	IEnumerator SpawnTarget()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(spawnRate);
+			int index = Random.Range(0, targets.Count);
+			Instantiate(targets[index]);
+		}
 	}
 
 	// Update is called once per frame
