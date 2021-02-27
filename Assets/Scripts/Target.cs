9c1815d9ac70c3422f553e7fcde25a1657c85c9c
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-02-26 20:57:10 -0500

    add gen script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Rigidbody targetRb;
+	// Start is called before the first frame update
+	void Start()
+	{
+		targetRb = GetComponent<Rigidbody>();
+		targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+		targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+		transform.position = new Vector3(Random.Range(-4, 4), -6);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
