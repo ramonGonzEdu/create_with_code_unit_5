a344f08e522b65af2f524db14cfecfa5499f2589
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-03-05 19:38:46 -0500

    modify gamemanager to change text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 	public List<GameObject> targets;
+	public TextMeshProUGUI scoreText;
 	private float spawnRate = 1.0f;
+	private int score = 0;
+	private string scoreTextDef = "";
 	// Start is called before the first frame update
 	void Start()
 	{
 		StartCoroutine(SpawnTarget());
+		scoreTextDef = scoreText.text;
+		scoreText.text = scoreTextDef + score;
 	}
 
 	IEnumerator SpawnTarget()
