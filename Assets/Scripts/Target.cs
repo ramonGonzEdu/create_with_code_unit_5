36bcd659fee117de413fa83a33bdbf3dffe9d8dc
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-03-05 19:47:56 -0500

    add particle booms

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
 	private float xRange = 4;
 	private float ySpawnPos = -6;
 	public int pointValue = 0;
+	public GameObject explosionParticle;
 	private GameManager gameManager;
 	// Start is called before the first frame update
 	void Start()
@@ -40,6 +41,7 @@ public class Target : MonoBehaviour
 	private void OnMouseDown()
 	{
 		Destroy(gameObject);
+		Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
 		gameManager.UpdateScore(pointValue);
 	}
 
